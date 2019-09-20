1c4e5e65c360ef1a58fea1b1b058ca6d5a3b66f3
Kipiatkov Evgenii <kipiatkov.ev@phystech.edu>
2019-09-20 12:35:12 +0300

    Start to "draw cat". Add file picture_8_1_cat.py

---
+++ lab3/picture_8_1_cat.py
@@ -0,0 +1,12 @@
+# All rights is not reserved. Read ./COPYRIGHT
+
+# Draw "cat with threads in room"
+
+from graph import *
+import math
+
+width, height = windowSize()
+centre = width / 2, height / 2
+
+if __name__ == '__main__':
+	run()
