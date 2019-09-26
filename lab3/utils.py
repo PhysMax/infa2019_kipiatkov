fd3e4d7c538839e8b23144afa0033c6d3c4ee0ef
Kipiatkov Evgenii <kipiatkov.ev@phystech.edu>
2019-09-26 17:16:08 +0300

    Add database of colors in lab3/utils.py

--- lab3/utils.py
+++ lab3/utils.py
@@ -5,6 +5,17 @@
 from graph import *
 import math
 
+# Colors in RGB
+op_clr = 250, 150, 100 # Pink with a little orange
+grey_clr = 70, 70, 70 # Light-grey
+mg_clr = 100, 85, 0 # Marshy green
+mryel_clr = 180, 150, 0 # Marshy yellow
+pnk_clr = 250, 200, 200 # Pink
+gr_clr = 50, 200, 0 # Green with a little yellow
+bl_clr = 150, 250, 250 # Light-blue
+wh_clr = 255, 255, 255 # White
+blk_clr = 0, 0, 0 # Black
+
 C = canvas()
 
 def draw_oval(x, y, a, b)->'int':
